1d8e7bd17e896d0beab4c6a4f460d16e412c01cf
Kevin Csellak <kevincsellak@gmail.com>
2024-05-10 18:14:43 +0200

    created simulation-running folder with improved code

--- single_focus_investigations/fsu_range_visualize.py
+++ single_focus_investigations/fsu_range_visualize.py
@@ -103,8 +103,8 @@ def decide_color(variables, is_focus, extrema_variables):
     # TODO: use OKLAB or something similar and make sure the hues are evenly spaced.
     
 
-def visualize_simple_unreduced_3(f, initials_list, coefficients_list, interval_list, evaluations_list, solution_list, is_focus, extrema_variables, *, suppress_legend=False, suppress_ghosts=False):
-    fig, axs = plt.subplots(1, 2, layout='constrained')
+def visualize(f, initials_list, coefficients_list, interval_list, evaluations_list, solution_list, is_focus, extrema_variables, *, suppress_legend=False, suppress_ghosts=False):
+    fig, axs = plt.subplots(3, 2, layout='constrained')
     if len(solution_list) == 0:
         the_subtitle = ""
     for n in range(len(solution_list)):
@@ -119,8 +119,12 @@ def visualize_simple_unreduced_3(f, initials_list, coefficients_list, interval_l
             the_subtitle = make_subtitle(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus)
         the_label = make_label(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus)
         the_color = decide_color([p0, q0, x0, y0, r0, s0, k1, k2, k3, k4], is_focus, extrema_variables)
-        axs[0].plot(t, x, label=the_label, linewidth=1.5, color=the_color)
-        axs[1].plot(t, y, label=the_label, linewidth=1.5, color=the_color)
+        axs[0, 0].plot(t, p, label=the_label, linewidth=1.5, color=the_color)
+        axs[0, 1].plot(t, q, label=the_label, linewidth=1.5, color=the_color)
+        axs[1, 0].plot(t, x, label=the_label, linewidth=1.5, color=the_color)
+        axs[1, 1].plot(t, y, label=the_label, linewidth=1.5, color=the_color)
+        axs[2, 0].plot(t, r, label=the_label, linewidth=1.5, color=the_color)
+        axs[2, 1].plot(t, s, label=the_label, linewidth=1.5, color=the_color)
     if not suppress_ghosts:
         for n in range(len(solution_list)):
             initials_out = initials_list[n]
@@ -132,23 +136,50 @@ def visualize_simple_unreduced_3(f, initials_list, coefficients_list, interval_l
             k1, k2, k3, k4 = coefficients_out
             the_label = make_label(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus)
             the_color = decide_color([p0, q0, x0, y0, r0, s0, k1, k2, k3, k4], is_focus)
-            axs[0].plot(t, y, label=the_label, alpha=0.2, linewidth=1.5, color=the_color)  # for comparison
-            axs[1].plot(t, x, label=the_label, alpha=0.2, linewidth=1.5, color=the_color)  # for comparison
+            axs[1, 0].plot(t, y, label=the_label, alpha=0.2, linewidth=1.5, color=the_color)  # for comparison
+            axs[1, 1].plot(t, x, label=the_label, alpha=0.2, linewidth=1.5, color=the_color)  # for comparison
             # TODO: make the color of these lines appear in the legend too
             # TODO: make these lines appear behind the other ones, but with these colors
     # TODO: make it so the labels are aligned with one another
-    axs[0].set(ylabel="x", xlabel='t')
-    axs[0].grid(True, linestyle='dashed')
-    axs[1].set(ylabel="y", xlabel='t')
-    axs[1].grid(True, linestyle='dashed')
-    x_min, x_max = axs[0].get_ylim()
-    y_min, y_max = axs[1].get_ylim()
-    axs[0].set_ylim(min(x_min, y_min), max(x_max, y_max))
-    axs[1].set_ylim(min(x_min, y_min), max(x_max, y_max))
+    axs[0, 0].set(ylabel="p")
+    axs[0, 0].xaxis.set_major_formatter('')
+    axs[0, 0].tick_params(axis='x', length=0)
+    axs[0, 0].grid(True, linestyle='dashed')
+    axs[0, 1].set(ylabel="q")
+    axs[0, 1].xaxis.set_major_formatter('')
+    axs[0, 1].tick_params(axis='x', length=0)
+    axs[0, 1].grid(True, linestyle='dashed')
+    axs[1, 0].set(ylabel="x")
+    axs[1, 0].xaxis.set_major_formatter('')
+    axs[1, 0].tick_params(axis='x', length=0)
+    axs[1, 0].grid(True, linestyle='dashed')
+    axs[1, 1].set(ylabel="y")
+    axs[1, 1].xaxis.set_major_formatter('')
+    axs[1, 1].tick_params(axis='x', length=0)
+    axs[1, 1].grid(True, linestyle='dashed')
+    axs[2, 0].set(ylabel="r", xlabel='t')
+    axs[2, 0].grid(True, linestyle='dashed')
+    axs[2, 1].set(ylabel="s", xlabel='t')
+    axs[2, 1].grid(True, linestyle='dashed')
+    p_min, p_max = axs[0, 0].get_ylim()
+    q_min, q_max = axs[0, 1].get_ylim()
+    x_min, x_max = axs[1, 0].get_ylim()
+    y_min, y_max = axs[1, 1].get_ylim()
+    r_min, r_max = axs[2, 0].get_ylim()
+    s_min, s_max = axs[2, 1].get_ylim()
+    axs[0, 0].set_ylim(0, 2 * p0)
+    axs[0, 1].set_ylim(0, 2 * q0)
+    if not suppress_ghosts:
+        axs[1, 0].set_ylim(min(x_min, y_min), max(x_max, y_max))
+        axs[1, 1].set_ylim(min(x_min, y_min), max(x_max, y_max))
+    axs[2, 0].set_ylim(r_min, r_max)
+    axs[2, 1].set_ylim(s_min, s_max)
     print("graph success")
-    handles, labels = axs[0].get_legend_handles_labels()
+    handles, labels = axs[2, 0].get_legend_handles_labels()
     if not suppress_legend:
         fig.legend(handles, labels, mode='expand', loc='outside lower center', ncols=3, fontsize="small")
-    the_title = "A graph of the unreduced base model with constant P and Q."
+    the_title = "A graph of the full simple unreduced model."
     fig.suptitle(the_title + "\n" + the_subtitle)
     plt.show()
+
+# TODO: make it so the names of the variables and parameters are passed along so the label makers are general.
--- single_focus_investigations/tsu_range_visualize.py
+++ single_focus_investigations/tsu_range_visualize.py
@@ -1,94 +1,79 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as clr
 
-def make_subtitle(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus):
+
+def make_subtitle(x0, y0, k1, k2, k3, k4, p, q, is_focus):
     result = ""
     if not is_focus[0]:
         if result != "":
             result += "; "
-        result += f"{p0=}"
+        result += f"{x0=}"
     if not is_focus[1]:
         if result != "":
             result += "; "
-        result += f"{q0=}"
+        result += f"{y0=}"
     if not is_focus[2]:
         if result != "":
             result += "; "
-        result += f"{x0=}"
+        result += f"{k1=}"
     if not is_focus[3]:
         if result != "":
             result += "; "
-        result += f"{y0=}"
+        result += f"{k2=}"
     if not is_focus[4]:
         if result != "":
             result += "; "
-        result += f"{r0=}"
+        result += f"{k3=}"
     if not is_focus[5]:
         if result != "":
             result += "; "
-        result += f"{s0=}"
+        result += f"{k4=}"
     if not is_focus[6]:
         if result != "":
             result += "; "
-        result += f"{k1=}"
+        result += f"{p=}"
     if not is_focus[7]:
         if result != "":
             result += "; "
-        result += f"{k2=}"
-    if not is_focus[8]:
-        if result != "":
-            result += "; "
-        result += f"{k3=}"
-    if not is_focus[9]:
-        if result != "":
-            result += "; "
-        result += f"{k4=}"
+        result += f"{q=}"
 
     return result
 
 
-def make_label(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus):
+def make_label(x0, y0, k1, k2, k3, k4, p, q, is_focus):
     result = ""
     if is_focus[0]:
         if result != "":
             result += "; "
-        result += f"{p0=}"
+        result += f"{x0=}"
     if is_focus[1]:
         if result != "":
             result += "; "
-        result += f"{q0=}"
+        result += f"{y0=}"
     if is_focus[2]:
         if result != "":
             result += "; "
-        result += f"{x0=}"
+        result += f"{k1=}"
     if is_focus[3]:
         if result != "":
             result += "; "
-        result += f"{y0=}"
+        result += f"{k2=}"
     if is_focus[4]:
         if result != "":
             result += "; "
-        result += f"{r0=}"
+        result += f"{k3=}"
     if is_focus[5]:
         if result != "":
             result += "; "
-        result += f"{s0=}"
+        result += f"{k4=}"
     if is_focus[6]:
         if result != "":
             result += "; "
-        result += f"{k1=}"
+        result += f"{p=}"
     if is_focus[7]:
         if result != "":
             result += "; "
-        result += f"{k2=}"
-    if is_focus[8]:
-        if result != "":
-            result += "; "
-        result += f"{k3=}"
-    if is_focus[9]:
-        if result != "":
-            result += "; "
-        result += f"{k4=}"
+        result += f"{q=}"
 
     return result
 
@@ -103,7 +88,7 @@ def decide_color(variables, is_focus, extrema_variables):
     # TODO: use OKLAB or something similar and make sure the hues are evenly spaced.
     
 
-def visualize_simple_unreduced_3(f, initials_list, coefficients_list, interval_list, evaluations_list, solution_list, is_focus, extrema_variables, *, suppress_legend=False, suppress_ghosts=False):
+def visualize(f, initials_list, coefficients_list, interval_list, evaluations_list, solution_list, is_focus, extrema_variables, *, suppress_legend=False, suppress_ghosts=False):
     fig, axs = plt.subplots(1, 2, layout='constrained')
     if len(solution_list) == 0:
         the_subtitle = ""
@@ -112,13 +97,13 @@ def visualize_simple_unreduced_3(f, initials_list, coefficients_list, interval_l
         coefficients_out = coefficients_list[n]
         solution_out = solution_list[n]
         t = solution_out.t
-        p, q, x, y, r, s = solution_out.y
-        p0, q0, x0, y0, r0, s0 = initials_out
-        k1, k2, k3, k4 = coefficients_out
+        x, y = solution_out.y
+        x0, y0 = initials_out
+        k1, k2, k3, k4, p, q = coefficients_out
         if n == 1:
-            the_subtitle = make_subtitle(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus)
-        the_label = make_label(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus)
-        the_color = decide_color([p0, q0, x0, y0, r0, s0, k1, k2, k3, k4], is_focus, extrema_variables)
+            the_subtitle = make_subtitle(x0, y0, k1, k2, k3, k4, p, q, is_focus)
+        the_label = make_label(x0, y0, k1, k2, k3, k4, p, q, is_focus)
+        the_color = decide_color([x0, y0, k1, k2, k3, k4, p, q], is_focus, extrema_variables)
         axs[0].plot(t, x, label=the_label, linewidth=1.5, color=the_color)
         axs[1].plot(t, y, label=the_label, linewidth=1.5, color=the_color)
     if not suppress_ghosts:
@@ -127,11 +112,11 @@ def visualize_simple_unreduced_3(f, initials_list, coefficients_list, interval_l
             coefficients_out = coefficients_list[n]
             solution_out = solution_list[n]
             t = solution_out.t
-            p, q, x, y, r, s = solution_out.y
-            p0, q0, x0, y0, r0, s0 = initials_out
-            k1, k2, k3, k4 = coefficients_out
-            the_label = make_label(p0, q0, x0, y0, r0, s0, k1, k2, k3, k4, is_focus)
-            the_color = decide_color([p0, q0, x0, y0, r0, s0, k1, k2, k3, k4], is_focus)
+            x, y = solution_out.y
+            x0, y0 = initials_out
+            k1, k2, k3, k4, p, q = coefficients_out
+            the_label = make_label(x0, y0, k1, k2, k3, k4, p, q, is_focus)
+            the_color = decide_color([x0, y0, k1, k2, k3, k4, p, q], is_focus, extrema_variables)
             axs[0].plot(t, y, label=the_label, alpha=0.2, linewidth=1.5, color=the_color)  # for comparison
             axs[1].plot(t, x, label=the_label, alpha=0.2, linewidth=1.5, color=the_color)  # for comparison
             # TODO: make the color of these lines appear in the legend too
@@ -143,12 +128,15 @@ def visualize_simple_unreduced_3(f, initials_list, coefficients_list, interval_l
     axs[1].grid(True, linestyle='dashed')
     x_min, x_max = axs[0].get_ylim()
     y_min, y_max = axs[1].get_ylim()
-    axs[0].set_ylim(min(x_min, y_min), max(x_max, y_max))
-    axs[1].set_ylim(min(x_min, y_min), max(x_max, y_max))
+    if not suppress_ghosts:
+        axs[0].set_ylim(min(x_min, y_min), max(x_max, y_max))
+        axs[1].set_ylim(min(x_min, y_min), max(x_max, y_max))
     print("graph success")
     handles, labels = axs[0].get_legend_handles_labels()
     if not suppress_legend:
         fig.legend(handles, labels, mode='expand', loc='outside lower center', ncols=3, fontsize="small")
-    the_title = "A graph of the unreduced base model with constant P and Q."
+    the_title = "A graph of the truncated simple unreduced model."
     fig.suptitle(the_title + "\n" + the_subtitle)
     plt.show()
+
+# TODO: make it so the names of the variables and parameters are passed along so the label makers are general.
